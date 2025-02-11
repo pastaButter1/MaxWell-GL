2a84f374c2322946e6994c1887e9c01f5a131920
pastaButter1 <alexandre.lafreniere.2006@gmail.com>
2025-02-11 15:33:19 -0500

    Modifs

--- MaxWell-GL/MaxWell-GL/Moteur Graphique/Vertexarray/Vertexarray.cpp
+++ MaxWell-GL/MaxWell-GL/Moteur Graphique/Vertexarray/Vertexarray.cpp
@@ -7,6 +7,7 @@ void Vertexarray::generer(Vertexarray* const vao)
 
 void Vertexarray::ajouterAttribut(Vertexarray vao, uint32_t index, uint32_t taille, uint32_t type, bool normalizer, uint32_t pas, uint32_t decalage)
 {
+	lier(vao);
 	APPEL_GX(glEnableVertexAttribArray(index));
 	APPEL_GX(glVertexAttribPointer(index, taille, type, normalizer, pas, (void*)decalage));
 }

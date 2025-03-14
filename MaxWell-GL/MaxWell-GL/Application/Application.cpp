5390c4eb9dbaadd3956df8913c67f28875c1208c
pastaButter1 <alexandre.lafreniere.2006@gmail.com>
2025-03-14 13:32:35 -0400

    Réparation d'un problème dans les méthode rechercherIndex

---
+++ MaxWell-GL/MaxWell-GL/Application/Application.cpp
@@ -0,0 +1,212 @@
+#include "Application.h"
+#include "Moteur Graphique/Graphique.h"
+#include "Lib/GLFW/include/GLFW/glfw3.h"
+
+#include "Lib/IMGUI/IMGUI/imgui.h"
+#include "Lib/IMGUI/IMGUI/imgui_impl_glfw.h"
+#include "Lib/IMGUI/IMGUI/imgui_impl_opengl3.h"
+
+#include "Utilitaire.h"
+
+#include <conio.h>
+#include <chrono>
+
+void Application::initialiser(Application* const app, glm::uvec2 tailleFenetre)
+{
+	app->tailleFenetre = tailleFenetre;
+
+	if (app->fenetre != nullptr)
+	{
+		afficherErreur("Le pointeur de la fenetre GLFW devrait etre egal a nullptr");
+	}
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Création de la fenêtre avec GLFW
+
+	app->fenetre = glfwCreateWindow(app->tailleFenetre.x, app->tailleFenetre.y, "MaxWell-GL", NULL, NULL);
+	if (app->fenetre == nullptr)
+	{
+		afficherErreur("Erreur dans la creation de la fenetre GLFW");
+
+		glfwTerminate();
+
+		_getch();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(app->fenetre);
+
+	glfwSwapInterval(1);
+
+	afficherLog("Creation de la fenetre avec GLFW");
+
+	glfwMakeContextCurrent(app->fenetre);
+
+	if (glewInit() != GLEW_OK) {
+		afficherErreur("L'initialisation de GLEW a echoue");
+
+		_getch();
+		exit(-1);
+	}
+
+	afficherLog("Initialisation de GLEW");
+
+	afficherLog("OPENGL vendor : %s", glGetString(GL_VENDOR));
+	afficherLog("OPENGL renderer : %s", glGetString(GL_RENDERER));
+	afficherLog("OPENGL version : %s", glGetString(GL_VERSION));
+	afficherLog("OPENGL Supported glsl version : %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	//io->ConfigFlags |= ImGuiConfigFlags_::ImGuiConfigFlags_NavEnableKeyboard;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(app->fenetre, true);
+	ImGui_ImplOpenGL3_Init("#version 450");
+
+	//ImFont* dfont = io->Fonts->AddFontDefault();
+	app->police = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\micross.ttf", 18.0f);
+
+	// Style
+
+	ImGuiStyle* style = &ImGui::GetStyle();
+
+	style->WindowPadding = ImVec2(15, 15);
+	style->WindowRounding = 5.0f;
+	style->FramePadding = ImVec2(5, 5);
+	style->FrameRounding = 4.0f;
+	style->ItemSpacing = ImVec2(12, 8);
+	style->ItemInnerSpacing = ImVec2(8, 6);
+	style->IndentSpacing = 25.0f;
+	style->ScrollbarSize = 15.0f;
+	style->ScrollbarRounding = 9.0f;
+	style->GrabMinSize = 5.0f;
+	style->GrabRounding = 3.0f;
+	style->TabRounding = 0.5f;
+
+	style->Colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	style->Colors[ImGuiCol_TextDisabled] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+	style->Colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+	style->Colors[ImGuiCol_ChildBg] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+	style->Colors[ImGuiCol_PopupBg] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+	style->Colors[ImGuiCol_Border] = ImVec4(0.80f, 0.80f, 0.83f, 0.88f);
+	style->Colors[ImGuiCol_BorderShadow] = ImVec4(0.92f, 0.91f, 0.88f, 0.00f);
+	style->Colors[ImGuiCol_FrameBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+	style->Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+	style->Colors[ImGuiCol_FrameBgActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_TitleBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+	style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+	style->Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 0.98f, 0.95f, 0.75f);
+	style->Colors[ImGuiCol_MenuBarBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+	style->Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+	style->Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+	style->Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+	style->Colors[ImGuiCol_CheckMark] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+	style->Colors[ImGuiCol_SliderGrab] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+	style->Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+	style->Colors[ImGuiCol_Button] = ImVec4(0.392f, 0.0f, 0.64f, 1.00f);
+	style->Colors[ImGuiCol_ButtonHovered] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+	style->Colors[ImGuiCol_ButtonActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_Header] = ImVec4(0.30f, 0.30f, 0.30f, 1.00f);
+	style->Colors[ImGuiCol_HeaderHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_HeaderActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+	style->Colors[ImGuiCol_Separator] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+	style->Colors[ImGuiCol_SeparatorActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_ResizeGrip] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	style->Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	style->Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+	style->Colors[ImGuiCol_Tab] = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+	style->Colors[ImGuiCol_TabHovered] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f); // When hover window title
+	style->Colors[ImGuiCol_TabActive] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f); // Active window in selection
+	style->Colors[ImGuiCol_TabUnfocused] = ImVec4(0.10f, 0.10f, 0.10f, 1.00f); // Window not active in selection
+	style->Colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	style->Colors[ImGuiCol_DockingPreview] = ImVec4(0.25f, 0.39f, 0.90f, 1.00f);
+	style->Colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.05f, 0.05f, 0.05f, 1.00f);
+	style->Colors[ImGuiCol_PlotLines] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+	style->Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_PlotHistogram] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+	style->Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TableHeaderBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TableBorderStrong] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TableBorderLight] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TableRowBgAlt] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.43f);
+	style->Colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 0.80f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_NavHighlight] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_NavWindowingHighlight] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style->Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+}
+
+void Application::executer(const Application& app)
+{
+	auto tAvant = std::chrono::high_resolution_clock::now();
+
+	while (!glfwWindowShouldClose(app.fenetre))
+	{
+		auto tMaintenant = std::chrono::high_resolution_clock::now();
+		const float dt = (tMaintenant - tAvant).count() / 1000000000.0f;
+		tAvant = tMaintenant;
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar;
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+		ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_::ImGuiDockNodeFlags_None;
+
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::SetNextWindowViewport(viewport->ID);
+
+		ImGui::Begin("Dockspace window", 0, window_flags);
+
+		ImGuiIO& io = ImGui::GetIO();
+
+		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+		{
+			ImGuiID dockspace = ImGui::GetID("MyDockSpace");
+			ImGui::DockSpace(dockspace, ImVec2(0, 0), dockspace_flags);
+		}
+
+		ImGui::End();
+
+		ImGui::Begin("fenetre");
+
+		static glm::vec4 couleur;
+
+		ImGui::ColorEdit4("Couleur", (float*)&couleur);
+
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(app.fenetre);
+		glfwPollEvents();
+	}
+}
+
+void Application::fermer(Application* const app)
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(app->fenetre);
+	glfwTerminate();
+
+	app->fenetre = nullptr;
+}
---
+++ MaxWell-GL/MaxWell-GL/Moteur Graphique/MoteurGx/ListeIndexUnique.h
@@ -0,0 +1,153 @@
+#pragma once
+
+#include <vector>
+
+template<typename TypeCompteur, typename Contenant>
+class ListeIndexUnique
+{
+	std::vector<TypeCompteur> tamponIndex;
+	std::vector<Contenant> tamponDonnees;
+	uint32_t compteurTotal = 0;
+
+	const TypeCompteur compteurMax = (~(TypeCompteur)0); 
+
+public:
+	ListeIndexUnique()
+	{
+
+	}
+
+	void reserverLongueur(const uint32_t longueur)
+	{
+		if (longueur <= tamponIndex.size())
+			return;
+
+		tamponIndex.reserve(longueur);
+		tamponDonnees.reserve(longueur);
+	}
+
+	TypeCompteur ajouter()
+	{
+		TypeCompteur indexUnique = compteurTotal;
+
+		if (compteurTotal < compteurMax)
+		{
+			compteurTotal++;
+			tamponIndex.emplace_back(indexUnique);
+			tamponDonnees.emplace_back();
+		}
+
+		return indexUnique;
+	}
+
+	TypeCompteur ajouter(const Contenant& item)
+	{
+		TypeCompteur indexUnique = ajouter();
+
+		if (indexUnique < compteurMax)
+		{
+			tamponDonnees[tamponDonnees.size() - 1] = item;
+		}
+
+		return indexUnique;
+	}
+
+	void retirerIndex(const TypeCompteur index)
+	{
+		if (index < tamponIndex.size())
+		{
+			for (int i = index; i < tamponIndex.size() - 1; i++)
+			{
+				tamponIndex[i] = tamponIndex[i + 1];
+			}
+
+			for (int i = index; i < tamponIndex.size() - 1; i++)
+			{
+				tamponDonnees[i] = tamponDonnees[i + 1];
+			}
+
+			tamponIndex.pop_back();
+			tamponDonnees.pop_back();
+		}
+	}
+
+	void retirerIndexUnique(const TypeCompteur indexUnique)
+	{
+		TypeCompteur index = retournerIndex(indexUnique);
+
+		if (index < compteurMax)
+		{
+			retirerIndex(index);
+		}
+	}
+
+	const TypeCompteur retournerIndex(const TypeCompteur indexUnique) const
+	{
+		if (indexUnique >= compteurTotal)
+			return compteurMax;
+
+		const uint32_t tailleTampon = tamponIndex.size();
+		const uint32_t essaiDepart = (uint32_t)indexUnique * tailleTampon / compteurTotal;
+
+		uint32_t essai = essaiDepart < tailleTampon ? essaiDepart : tailleTampon;
+		uint32_t resultat = compteurMax;
+		uint32_t essaiMin = 0;
+		uint32_t essaiMax = tailleTampon;
+		uint32_t dst = 0;
+
+		// Recherche binaire
+		// Les indexes sont toutes en ordre croissant
+
+		while (resultat != indexUnique && essaiMax - essaiMin > 1)
+		{
+			resultat = tamponIndex[essai];
+
+			if (resultat < indexUnique)
+			{
+				dst = essaiMax - essai;
+
+				essaiMin = essai;
+
+				essai += dst >> 1;
+			}
+
+			else if (resultat > indexUnique)
+			{
+				dst = essai - essaiMin;
+
+				essaiMax = essai;
+
+				essai -= dst >> 1;
+			}
+		}
+
+		return essaiMax - essaiMin <= 1 ? compteurMax : essai;
+	}
+
+	Contenant& rechercherIndex(const TypeCompteur index)
+	{
+		if (index < tamponDonnees.size() - 1)
+		{
+			return tamponDonnees[index];
+		}
+
+		Contenant* const ptrNull = nullptr;
+
+		return *ptrNull;
+	}
+
+	Contenant& rechercherIndexUnique(const TypeCompteur indexUnique)
+	{
+		return rechercherIndex(retournerIndex(indexUnique));
+	}
+
+	const Contenant& rechercherIndex(const TypeCompteur index) const
+	{
+		return ((ListeIndexUnique < TypeCompteur, Contenant>*)this)->rechercherIndex(index);
+	}
+
+	const Contenant& rechercherIndexUnique(const TypeCompteur indexUnique) const
+	{
+		return ((ListeIndexUnique < TypeCompteur, Contenant>*)this)->rechercherIndexUnique(indexUnique);
+	}
+};
